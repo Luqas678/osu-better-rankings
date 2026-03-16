faf8f65789554dd0f99083b4b040d98b0eb207cf
Luqas678 <lucasiniesta478@gmail.com>
2026-03-16 15:29:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  // SUSTITUYE esto con el nombre exacto de tu repositorio entre barras
+
   base: "/osu-better-rankings/", 
   server: {
     host: "0.0.0.0",
